df9384ddbde6ad93d5a95ebb57e636d3893493a5
shahabsharafi <shahab.sharafi@gmail.com>
2017-06-02 01:40:30 -0700

    profile begin

---
+++ FAQPhone/FAQPhone/Views/ProfilePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FAQPhone.Views
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+            BindingContext = new ContentPageViewModel();
+        }
+    }
+
+    class ProfilePageViewModel : INotifyPropertyChanged
+    {
+
+        public ProfilePageViewModel()
+        {
+            IncreaseCountCommand = new Command(IncreaseCount);
+        }
+
+        int count;
+
+        string countDisplay = "You clicked 0 times.";
+        public string CountDisplay
+        {
+            get { return countDisplay; }
+            set { countDisplay = value; OnPropertyChanged(); }
+        }
+
+        public ICommand IncreaseCountCommand { get; }
+
+        void IncreaseCount() =>
+            CountDisplay = $"You clicked {++count} times";
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
