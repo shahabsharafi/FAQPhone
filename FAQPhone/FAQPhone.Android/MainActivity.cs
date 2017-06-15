265d81444b206845014d370cdf623a31859e2e9a
shahabsharafi <shahab.sharafi@gmail.com>
2017-06-15 14:14:12 -0700

    android splash screen and icon

--- FAQPhone/FAQPhone.Android/MainActivity.cs
+++ FAQPhone/FAQPhone.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace FAQPhone.Droid
 {
-    [Activity(Label = "FAQPhone", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FAQPhone", Icon = "@drawable/appicon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
