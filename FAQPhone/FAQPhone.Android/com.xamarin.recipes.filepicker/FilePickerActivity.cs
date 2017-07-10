c9c7ae2f12067d2f334b44ea4e524cccd5c269e8
shahabsharafi <shahab.sharafi@gmail.com>
2017-07-10 10:47:23 -0700

    file manager begin

---
+++ FAQPhone/FAQPhone.Android/com.xamarin.recipes.filepicker/FilePickerActivity.cs
@@ -0,0 +1,17 @@
+﻿namespace com.xamarin.recipes.filepicker
+{
+    using Android.App;
+    using Android.OS;
+    using Android.Support.V4.App;
+    using FAQPhone.Droid;
+
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/appicon")]
+    public class FilePickerActivity : FragmentActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
