8149c38507fef9ce209b9cec2802bbbb5f6c4494
shahabsharafi <shahab.sharafi@gmail.com>
2017-11-11 09:05:29 -0800

    some change for release

--- FAQPhone/FAQPhone/Resx/AppResources.Designer.cs
+++ FAQPhone/FAQPhone/Resx/AppResources.Designer.cs
@@ -20,7 +20,7 @@ namespace FAQPhone.Resx {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class AppResources {
@@ -152,7 +152,7 @@ namespace FAQPhone.Resx {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.6.2.
+        ///   Looks up a localized string similar to 0.7.0.
         /// </summary>
         internal static string app_version {
             get {
