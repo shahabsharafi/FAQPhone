456558fa0e4f41317e2d1233782980459f932c7d
shahabsharafi <shahab.sharafi@gmail.com>
2018-02-26 00:51:32 +0330

    change version

--- FAQPhone/FAQPhone/Resx/Ar.Designer.cs
+++ FAQPhone/FAQPhone/Resx/Ar.Designer.cs
@@ -170,7 +170,7 @@ namespace FAQPhone.Resx {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.8.5.
+        ///   Looks up a localized string similar to 0.8.7.
         /// </summary>
         internal static string app_version {
             get {
